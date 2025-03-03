7f9bca140a6edd1c1d7a697b830fdff662294a68
noor188 <noormmlk@gmail.com>
2025-03-03 15:34:13 -0700

    Ticket 2: Create the Task Entity

---
+++ src/main/java/com/Sys/Task_Managment_System_api/model/Task.java
@@ -0,0 +1,34 @@
+package com.Sys.Task_Managment_System_api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.Objects;
+
+@Entity
+@Data
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String description;
+    private String status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
