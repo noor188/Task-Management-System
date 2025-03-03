c087447ce11632d5df3812d8754111980ed218ad
noor188 <noormmlk@gmail.com>
2025-03-03 15:14:17 -0700

    Setting up Spring boot project

---
+++ src/test/java/com/Sys/Task_Managment_System_api/TaskManagmentSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sys.Task_Managment_System_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagmentSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
